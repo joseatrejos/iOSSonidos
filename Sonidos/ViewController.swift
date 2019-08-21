71c5a011a17a6f380b4e3af8a28228ed5b76e8c0
Alumno <ulsa@iMacD115.local>
2019-08-21 14:42:28 -0700

    Reproduccion de sonidos (hasta ahora 2)

--- Sonidos/ViewController.swift
+++ Sonidos/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player : AVAudioPlayer?
+    var player2 : AVAudioPlayer?
+    
+    @IBAction func doTapReproducir(_ sender: Any) {
+        if player != nil {
+            player?.play()
+        }
+    }
+    
+    @IBAction func doTapReproducir2(_ sender: Any) {
+        if player2 != nil {
+            player2?.play()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "AUDIO 29.wav", ofType: nil)
+        let url = URL(fileURLWithPath : path!)
+
+        // Try catch example (used as a do->try & catch in Swift)
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player2 = try AVAudioPlayer(contentsOf: url)
+        }
+        catch{
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
